d8100466b52242807f39cc2010d6212d347d5b7c
Craig Pryde <pryde3010@outlook.com>
2019-05-27 20:11:36 +0100

    Updated schedule and cleaned up dependencies

--- schedule.js
+++ schedule.js
@@ -1,18 +1,4 @@
 /* Dependencies */ 
-const Shell = require("./classes/Shell").Shell;
+const shell = require("shelljs");
 
-const shell = new Shell();
-
-// Set commands to run
-const commands = [
-    "npm run build"
-];
-
-// Run the shell commands
-shell.series(commands, function(error) {
-    if(error) {
-        console.log(error);
-        return;
-    }
-    console.log("Schedule set successfully!");
-});
+shell.exec("npm run build");
